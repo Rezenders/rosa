9d6af95e872819606273f0ada601accdd7e9f36d
Gustavo <g.rezendesilva@tudelft.nl>
2022-09-12 13:33:50 +0200

    ✨ geting components from task

--- src/metacontrol.py
+++ src/metacontrol.py
@@ -2,36 +2,82 @@
 
 from kb_interface import TypeDBInterface
 
-if __name__ == '__main__':
+# get leaf functions from task
+def get_leaf_functions_from_task(interface, task_name):
+    functions = interface.get_leaf_functions_from_task(task_name)
+    functions_dict = dict()
+    for f in functions:
+        root_function = f.get("function_name").get_value()
+        leaf_function = f.get("leaf_function_name").get_value()
+        if root_function in functions_dict:
+            functions_dict[root_function] += [leaf_function]
+        else:
+            functions_dict[root_function] = [leaf_function]
+    return functions_dict
+
+def get_required_components(interface, functions_dict):
+    components_dict = dict()
+    for key in functions_dict:
+        required_functions = functions_dict[key]
+        for function in required_functions:
+            function_designs = interface.get_function_designs_ordered(function)
+
+            #TODO: Don't throw exception when len=0. would be nice to get an explanition of why
+            function_design_name = function_designs[0].get("function_design_name").get_value() #TODO: check if feasible? order?
+            required_components =[]
+            required_components_type = interface.get_component_types_from_function_design(function_design_name)
+
+            for component_type in required_components_type:
+                _component_type_str = component_type.get("component_type").get_value()
+                _components_designs_ordered = interface.get_components_from_component_type(_component_type_str)
+                required_components += [{'component_name':_components_designs_ordered[0].get("component_name")}]
+
+            required_components += interface.get_components_from_function_design(function_design_name)
+            for component in required_components:
+                if function in components_dict:
+                    components_dict[function] += [component.get("component_name").get_value()]
+                else:
+                    components_dict[function] = [component.get("component_name").get_value()]
+    return components_dict
+
+def solve_task(interface, task_name):
+    print("Querying leaf functions of task ", task_name)
+    functions_dict = get_leaf_functions_from_task(typedb_interface, task_name)
+    print(functions_dict)
+
+    print("Querying required components for leaf functions")
+    components_dict = get_required_components(typedb_interface, functions_dict)
+    print(components_dict)
+
+    return components_dict
+
+def executor(interface, required_components):
+    print(required_components)
+    for components in required_components.values():
+        for component in components:
+            # component_executor = interface.get_component_executor(component).get('component_executor').get_value()
+            component_executor = interface.get_component_executor(component)
+            if len(component_executor)>0:
+                print(component_executor[0].get('component_executor').get_value())
+
+def init_typedb_interface(address, database_name, schema_path, data_path):
     typedb_interface = TypeDBInterface()
     typedb_interface.connect_client("localhost:1729")
     typedb_interface.create_database("pipeline_inspection", force=True)
     typedb_interface.load_schema("../typeDB/schema/schema.tql")
-    # typedb_interface.load_schema("example_application_schema.tql")
     typedb_interface.load_data("../typeDB/data/example_search_pipeline.tql", force=True)
-    # typedb_interface.get_ok_function_groundings()
-    leaf_functions = typedb_interface.get_leaf_functions('Navigate safely')
-    print('Must solve functions: ')
-    for function in leaf_functions:
-        function_name = function.get("function_name").get_value()
-        print('  Function:'+function_name)
-        function_designs = typedb_interface.get_function_designs_ordered(function_name)
-        for fd in function_designs:
-            fd_name = fd.get("function_design_name").get_value()
-            qa_type = fd.get("qa-type").get_value()
-            qa_value = fd.get("qa-value").get_value()
-            print('    Fd:'+fd_name,' ',qa_type, ':',qa_value)
-            components = typedb_interface.get_components_from_function_design(fd_name)
-            for c in components:
-                component_name = c.get("component_name").get_value()
-                print("      Component:", component_name)
+    return typedb_interface
 
-            component_types = typedb_interface.get_component_types_from_function_design(fd_name)
-            for ct in component_types:
-                component_type = ct.get("component_type").get_value()
-                print("      Component Type:", component_type)
-                components_from_type = typedb_interface.get_components_from_component_type(component_type)
-                for c in components_from_type:
-                    component_name = c.get("component_name").get_value()
-                    component_priority = c.get("priority").get_value()
-                    print("        Component:", component_name, " priority:", component_priority)
+if __name__ == '__main__':
+
+    typedb_interface = init_typedb_interface(
+        "localhost:1729",
+        "pipeline_inspection",
+        "../typeDB/schema/schema.tql",
+        "../typeDB/data/example_search_pipeline.tql")
+
+    task_name = 'Search pipeline'
+    print('Task: ',task_name)
+    required_components = solve_task(typedb_interface, task_name)
+
+    executor(typedb_interface, required_components)
