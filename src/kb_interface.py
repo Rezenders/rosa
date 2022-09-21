31230e6976f817e76d765e8c6fb3984d369053a2
Gustavo <g.rezendesilva@tudelft.nl>
2022-09-21 18:58:10 +0200

    🚧 bit of refactoring and adding new features

--- src/kb_interface.py
+++ src/kb_interface.py
@@ -125,52 +125,140 @@ class TypeDBInterface:
     # Read/write database end
 
     # Queries begining
-    def get_unsolved_required_tasks(self):
+    def get_unsolved_required_tasks_raw(self):
         query = f'''
             match
-                $task isa Task, has task-name $task-name, has is-task-required true;
-                not {{$task isa Task, has is-task-required true, has task-status $task-status;
-                $task-status = "activated";}};
+                $task isa Task, has task-name $task-name,
+                    has is-task-required true;
+                not {{$task isa Task, has is-task-required true,
+                    has task-status $task-status;
+                    $task-status = "activated";}};
                 get $task-name;
         '''
         return self.match_database(query)
 
-    def get_leaf_functions_from_task(self, task_name):
+    def get_unsolved_required_tasks(self):
+        query_result = self.get_unsolved_required_tasks_raw()
+        return [result.get("task-name").get_value() for result in query_result]
+
+    # def get_leaf_functions_from_task_raw(self, task_name):
+    #     query = f'''
+    #         match
+    #             $task isa Task, has task-name "{task_name}";
+    #             (task:$task, required-function:$function) isa task-requirement;
+    #             $function isa Function, has function-name $function_name;
+    #             $leaf_function_name isa function-name;
+    #             {{$function has is-leaf-function true,
+    #                 has function-name $leaf_function_name;}} or
+    #             {{(parent-function:$function, child-function:$fc)
+    #                 isa implicit-functional-hierarchy;
+    #                 $fc has is-leaf-function true,
+    #                 has function-name $leaf_function_name; }};
+    #             get $function_name, $leaf_function_name;
+    #     '''
+    #     return self.match_database(query)
+    #
+    # def get_leaf_functions_from_task(self, task_name):
+    #     query_result = self.get_leaf_functions_from_task_raw(task_name)
+    #     functions_dict = dict()
+    #
+    #     for function in query_result:
+    #         root_function = function.get("function_name").get_value()
+    #         leaf_function = function.get("leaf_function_name").get_value()
+    #         functions_dict.setdefault(root_function, []).append(leaf_function)
+    #
+    #     return functions_dict
+
+    # TODO: replace for implicit-task-requirement???
+    # TODO: I think it might be possible to just return a list with all
+    # functions, no need for a dict
+    def get_functional_hierarchy_from_task(self, task_name):
+        root_functions = self.get_root_functions_from_task(task_name)
+        return {f: [f] + self.get_child_functions(f) for f in root_functions}
+
+    def get_root_functions_from_task_raw(self, task_name):
         query = f'''
             match
                 $task isa Task, has task-name "{task_name}";
                 (task:$task, required-function:$function) isa task-requirement;
-                $function isa Function, has function-name $function_name;
-                $leaf_function_name isa function-name;
-                {{$function has is-leaf-function true, has function-name $leaf_function_name;}} or
-                {{(parent-function:$function, child-function:$fc) isa implicit-functional-hierarchy;
-                    $fc has is-leaf-function true, has function-name $leaf_function_name; }};
-                get $function_name, $leaf_function_name;
+                $function isa Function, has function-name $function-name;
+                get $function-name;
         '''
         return self.match_database(query)
 
-    def get_required_functions_from_task(self, task_name):
+    def get_root_functions_from_task(self, task_name):
+        query_result = self.get_root_functions_from_task_raw(task_name)
+        return [r.get("function-name").get_value() for r in query_result]
+
+    # def get_functional_hierarchy_from_task(self, task_name):
+    #     query_result = self.get_functional_hierarchy_from_task_raw(task_name)
+    #     functions_dict = dict()
+    #
+    #     for result in query_result:
+    #         root_function = result.get("function-name").get_value()
+    #         print(root_function)
+    #         leaf_function = result.get("fc-name").get_value()
+    #         print(leaf_function)
+    #         functions_dict.setdefault(root_function, []).append(leaf_function)
+    #
+    #     return functions_dict
+
+    def get_child_functions_raw(self, function):
         query = f'''
             match
-                $t isa Task, has task-name "{task_name}";
-                (task:$t, required-function:$function) isa task-requirement;
-                $function isa Function, has function-name $function_name;
-                get $function_name;
+                $function isa Function, has function-name "{function}";
+                (parent-function:$function, child-function:$fc)
+                    isa implicit-functional-hierarchy;
+                $fc isa Function, has function-name $fc-name;
+                get $fc-name;
         '''
         return self.match_database(query)
+
+    def get_child_functions(self, function):
+        query_result = self.get_child_functions_raw(function)
+        return [result.get("fc-name").get_value() for result in query_result]
+
+    # def get_required_functions_from_task(self, task_name):
+    #     query = f'''
+    #         match
+    #             $t isa Task, has task-name "{task_name}";
+    #             (task:$t, required-function:$function) isa task-requirement;
+    #             $function isa Function, has function-name $function_name;
+    #             get $function_name;
+    #     '''
+    #     return self.match_database(query)
+
+    def get_functions_not_required_anymore_raw(self):
+        query = f'''
+            match
+                $function isa Function, has is-function-required true,
+                    has function-name $function-name;
+                $task isa Task;
+                {{(task:$task, required-function:$function)
+                    isa implicit-task-requirement;}} or
+                {{(task:$task, required-function:$function)
+                    isa task-requirement;}};
+                not {{$task isa Task, has is-task-required true;}};
+                get $function-name;
+        '''
+        return self.match_database(query)
+
+    def get_functions_not_required_anymore(self):
+        query_result = self.get_functions_not_required_anymore_raw()
+        return [r.get("function-name").get_value() for r in query_result]
+
 
     # TODO: standard for variable names in queries
-    # TODO: remove unecessary variables from query
-    def get_leaf_functions(self, root_function_name):
-        query = f'''
-            match
-                $f isa Function, has function-name "{root_function_name}";
-                (parent-function:$f, child-function:$leaf_function) isa implicit-functional-hierarchy;
-                $leaf_function has function-name $function_name;
-                $leaf_function has is-leaf-function true;
-                get $leaf_function, $function_name;
-        '''
-        return self.match_database(query)
+    # def get_leaf_functions(self, root_function_name):
+    #     query = f'''
+    #         match
+    #             $f isa Function, has function-name "{root_function_name}";
+    #             (parent-function:$f, child-function:$leaf_function) isa implicit-functional-hierarchy;
+    #             $leaf_function has function-name $function_name;
+    #             $leaf_function has is-leaf-function true;
+    #             get $leaf_function, $function_name;
+    #     '''
+    #     return self.match_database(query)
 
     # fds ordered desc single qa
     def function_designs_order_desc(self, function_name):
@@ -178,10 +266,11 @@ class TypeDBInterface:
             match
                 $f isa Function, has function-name "{function_name}";
                 $fd (function:$f, required-component:$c) isa function-design;
-                $fd isa function-design, has function-design-name $function_design_name;
+                $fd isa function-design, has function-design-name $fd-name;
                 (function-design:$fd, qa:$eqa) isa estimated-qa;
-                $eqa isa EstimatedQualityAttribute, has qa-type $qa-type, has qa-value $qa-value;
-                get $function_design_name, $qa-type, $qa-value;
+                $eqa isa EstimatedQualityAttribute, has qa-type $qa-type,
+                    has qa-value $qa-value;
+                get $fd-name, $qa-type, $qa-value;
                 sort $qa-value desc;
         '''
         return self.match_database(query)
@@ -192,48 +281,68 @@ class TypeDBInterface:
             match
                 $f isa Function, has function-name "{function_name}";
                 $fd (function:$f, required-component:$c) isa function-design;
-                $fd isa function-design, has function-design-name $function_design_name;
+                $fd isa function-design, has function-design-name $fd-name;
                 (function-design:$fd, qa:$eqa) isa estimated-qa;
-                $eqa isa EstimatedQualityAttribute, has qa-type $qa-type, has qa-value $qa-value;
-                get $function_design_name, $qa-type, $qa-value;
+                $eqa isa EstimatedQualityAttribute, has qa-type $qa-type,
+                    has qa-value $qa-value;
+                get $fd-name, $qa-type, $qa-value;
                 sort $qa-value asc;
         '''
         return self.match_database(query)
 
     # get fd with higher estimated qa (only considering 1 qa)
-    def get_function_designs_ordered(self, function_name):
+    def get_function_designs_ordered_raw(self, function_name):
+        _func = None
         if function_name in self.function_designs_ordering_funcs:
-            return getattr(self, self.function_designs_ordering_funcs[function_name])(function_name)
+            _func = getattr(
+                self, self.function_designs_ordering_funcs[function_name])
         else:
-            return getattr(self, self.default_function_design_ordering_func)(function_name)
+            _func = getattr(self, self.default_function_design_ordering_func)
+
+        return _func(function_name)
+
+    def get_function_designs_ordered(self, function_name):
+        query_result = self.get_function_designs_ordered_raw(function_name)
+        return [r.get("fd-name").get_value() for r in query_result]
 
     # get components from fd (only dealing with Components, not ComponentType)
-    # TODO: get component-executor
-    def get_components_from_function_design(self, function_design_name):
+    def get_components_from_function_design_raw(self, fd_name):
         query = f'''
             match
-                $fd (function:$f, required-component:$component) isa function-design,  has function-design-name "{function_design_name}";
-                $component isa Component, has component-name $component_name;
-                get $component_name;
+                $fd (function:$f, required-component:$component)
+                    isa function-design,  has function-design-name "{fd_name}";
+                $component isa Component, has component-name $component-name;
+                get $component-name;
         '''
         return self.match_database(query)
 
-    def get_component_types_from_function_design(self, function_design_name):
+    def get_components_from_function_design(self, fd_name):
+        query_result = self.get_components_from_function_design_raw(fd_name)
+        return [r.get("component-name").get_value() for r in query_result]
+
+    def get_component_types_from_function_design_raw(self, fd_name):
         query = f'''
             match
-                $fd (function:$f, required-component:$component) isa function-design,  has function-design-name "{function_design_name}";
-                $component isa ComponentType, has component-type $component_type;
-                get $component_type;
+                $fd (function:$f, required-component:$c)
+                    isa function-design,  has function-design-name "{fd_name}";
+                $c isa ComponentType, has component-type $component-type;
+                get $component-type;
         '''
         return self.match_database(query)
+
+    def get_component_types_from_function_design(self, fd_name):
+        result = self.get_component_types_from_function_design_raw(fd_name)
+        return [r.get("component-type").get_value() for r in result]
 
     def component_order_asc(self, component_type):
         query = f'''
             match
                 $ct isa ComponentType, has component-type "{component_type}";
-                $cd (componentType:$ct, component:$component) isa component-design, has component-design-priority $priority;
-                $component isa Component, has component-name $component_name;
-                get $component_name, $priority;
+                $cd (componentType:$ct, component:$component)
+                    isa component-design,
+                    has component-design-priority $priority;
+                $component isa Component, has component-name $component-name;
+                get $component-name, $priority;
                 sort $priority asc;
         '''
         return self.match_database(query)
@@ -242,18 +351,27 @@ class TypeDBInterface:
         query = f'''
             match
                 $ct isa ComponentType, has component-type "{component_type}";
-                $cd (componentType:$ct, component:$component) isa component-design, has component-design-priority $priority;
-                $component isa Component, has component-name $component_name;
-                get $component_name, $priority;
+                $cd (componentType:$ct, component:$component)
+                    isa component-design,
+                    has component-design-priority $priority;
+                $component isa Component, has component-name $component-name;
+                get $component-name, $priority;
                 sort $priority desc;
         '''
         return self.match_database(query)
 
-    def get_components_from_component_type(self, component_type):
+    def get_components_from_component_type_raw(self, component_type):
+        _func = None
         if component_type in self.component_ordering_funcs:
-            return getattr(self, self.component_ordering_funcs[component_type])(component_type)
+            _func = getattr(
+                self, self.component_ordering_funcs[component_type])
         else:
-            return getattr(self, self.default_component_ordering_func)(component_type)
+            _func = getattr(self, self.default_component_ordering_func)
+        return _func(component_type)
+
+    def get_components_from_component_type(self, component_type):
+        result = self.get_components_from_component_type_raw(component_type)
+        return [r.get("component-name").get_value() for r in result]
 
     def delete_entity(self, entity, key, key_value):
         query = f'''
