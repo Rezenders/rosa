101d7f90b67d353249c5b23a9c810b7ae5c2c1e7
Gustavo <g.rezendesilva@tudelft.nl>
2022-09-21 18:59:13 +0200

    🚧 refactoring, using planner class

--- src/metacontrol.py
+++ src/metacontrol.py
@@ -1,55 +1,13 @@
 #!/usr/bin/env python
 import time
+from planner import Planner
 from executor import Executor
 from kb_interface import TypeDBInterface
 
-
-# get leaf functions from task
-def get_leaf_functions_from_task(interface, task_name):
-    functions = interface.get_leaf_functions_from_task(task_name)
-    functions_dict = dict()
-    for f in functions:
-        root_function = f.get("function_name").get_value()
-        leaf_function = f.get("leaf_function_name").get_value()
-        if root_function in functions_dict:
-            functions_dict[root_function] += [leaf_function]
-        else:
-            functions_dict[root_function] = [leaf_function]
-    return functions_dict
-
-def get_required_components(interface, functions_dict):
-    components_dict = dict()
-    for key in functions_dict:
-        required_functions = functions_dict[key]
-        for function in required_functions:
-            function_designs = interface.get_function_designs_ordered(function)
-
-            #TODO: Don't throw exception when len=0. would be nice to get an explanition of why
-            function_design_name = function_designs[0].get("function_design_name").get_value() #TODO: check if feasible? order?
-            required_components =[]
-            required_components_type = interface.get_component_types_from_function_design(function_design_name)
-
-            for component_type in required_components_type:
-                _component_type_str = component_type.get("component_type").get_value()
-                _components_designs_ordered = interface.get_components_from_component_type(_component_type_str)
-                component_name = _components_designs_ordered[0].get("component_name")
-                required_components += [{'component_name': component_name}]
-                interface.update_component_requirement(component_name, "true")
-
-            required_components += interface.get_components_from_function_design(function_design_name)
-            for component in required_components:
-                component_name = component.get("component_name").get_value()
-                interface.update_component_requirement(component_name, "true")
-                if function in components_dict:  # TODO: this can be improved
-                    components_dict[function] += [component_name]
-                else:
-                    components_dict[function] = [component_name]
-    return components_dict
-
-def solve_task(interface, task_name):
-    functions_dict = get_leaf_functions_from_task(typedb_interface, task_name)
-    components_dict = get_required_components(typedb_interface, functions_dict)
-    return components_dict
+# def solve_task(interface, task_name):
+#     functions_dict = get_leaf_functions_from_task(typedb_interface, task_name)
+#     components_dict = get_required_components(typedb_interface, functions_dict)
+#     return components_dict
 
 if __name__ == '__main__':
 
@@ -65,20 +23,20 @@ if __name__ == '__main__':
         function_designs_ordering_funcs=function_designs_ordering_funcs,
         component_ordering_funcs=component_ordering_funcs)
 
+    planner = Planner(typedb_interface)
     executor = Executor(typedb_interface)
-    task_name = 'Search pipeline'
-    print('Task: ', task_name)
 
     # Plan
-    required_components = solve_task(typedb_interface, task_name)
+    planner.plan()
+    # required_components = solve_task(typedb_interface, task_name)
 
     # Execute
-    executor.request_components(required_components)
+    # executor.request_components(required_components)
 
-    i = 0
-    while i < 20:
-        executor.monitor_components()
-        time.sleep(1)
-        i += 1
-
-    executor.stop_components(['Thruster controller 1'])
+    # i = 0
+    # while i < 20:
+    #     executor.monitor_components()
+    #     time.sleep(1)
+    #     i += 1
+    #
+    # executor.stop_components(['Thruster controller 1'])
