e07f6b0dd90f3065f81ce5507afc3ded6cdab4dc
Gustavo <g.rezendesilva@tudelft.nl>
2023-04-28 18:46:42 +0200

    💬 change model_interface name to kb_interface

--- src/test_typedb_interface.py
+++ src/test_typedb_interface.py
@@ -3,36 +3,36 @@ from typedb_interface import TypeDBInterface
 
 
 @pytest.fixture
-def model_interface():
+def kb_interface():
     function_designs_ordering_funcs = {
         'Execute and control AUV motion': 'function_designs_order_desc'}
     component_ordering_funcs = {'component type': 'component_order_desc'}
-    typedb_interface = TypeDBInterface(
+    kb_interface = TypeDBInterface(
         "localhost:1729",
         "test_database",
         "../typeDB/schema/uio_schema.tql",
         "../typeDB/data/empty.tql",  # TODO:better way to handle empty data
         force_database=True,
         force_data=True)
-    return typedb_interface
+    return kb_interface
 
 
-def test_insert_entity(model_interface):
-    model_interface.insert_entity('Task', 'task-name', 't1')
+def test_insert_entity(kb_interface):
+    kb_interface.insert_entity('Task', 'task-name', 't1')
     query = f'''
         match $entity isa Task, has task-name "t1";
         get $entity;
     '''
-    result = model_interface.match_database(query)
+    result = kb_interface.match_database(query)
     assert len(result) > 0
 
 
-def test_delete_entity(model_interface):
-    model_interface.insert_entity('Task', 'task-name', 't1')
-    model_interface.delete_entity('Task', 'task-name', 't1')
+def test_delete_entity(kb_interface):
+    kb_interface.insert_entity('Task', 'task-name', 't1')
+    kb_interface.delete_entity('Task', 'task-name', 't1')
     query = f'''
         match $entity isa Task, has task-name "t1";
         get $entity;
     '''
-    result = model_interface.match_database(query)
+    result = kb_interface.match_database(query)
     assert len(result) == 0
