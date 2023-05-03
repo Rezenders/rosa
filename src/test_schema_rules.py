493018fe9a45eb70c6c5eeb64d441dd874795f28
Gustavo <g.rezendesilva@tudelft.nl>
2023-05-03 15:54:29 +0200

    ✅ tests for status inference passing

--- src/test_schema_rules.py
+++ src/test_schema_rules.py
@@ -6,7 +6,7 @@ from typedb_interface import TypeDBInterface
 def kb_interface():
     kb_interface = TypeDBInterface(
         "localhost:1729",
-        "test_database",
+        "pytest_database",
         "../typeDB/schema/uio_schema.tql",
         "../typeDB/data/test_data.tql",  # TODO:better way to handle empty data
         force_database=True,
@@ -14,16 +14,119 @@ def kb_interface():
     return kb_interface
 
 
+@pytest.mark.parametrize("att_name, att_value, config_name, constrainment_status", [
+    ('ea1', '2.5', 'high param', 'violated'),
+    ('ea1', '2.5', 'low param', 'satisfied'),
+    ('ea1', '', 'low param', 'not evaluated'),
+])
+def test_constrainment_status_inference(
+        kb_interface, att_name, att_value, config_name, constrainment_status):
+
+    if att_value != '':
+        kb_interface.update_attribute_entity(
+            'Attribute',
+            'attribute-name',
+            att_name,
+            'attribute-measurement',
+            att_value)
+    query = f'''
+        match
+            $ea isa EnvironmentalAttribute, has attribute-name "{att_name}";
+            $config isa component-configuration, has component-configuration-name "{config_name}";
+            (constraint: $ea, constrained: $config) isa constrainment, has constrainment-status $status;
+            get $status;
+    '''
+    query_result = kb_interface.match_database(query)
+    inferred_status = [
+        status.get("status").get_value() for status in query_result]
+    assert inferred_status[0] == constrainment_status
+
+
+@pytest.mark.parametrize("att_name, att_value, config_name, config_status", [
+    ('ea1', '2.5', 'high param', 'unfeasible'),
+    ('ea1', '2.5', 'low param', 'feasible'),
+    ('ea1', '', 'low param', 'feasible'),
+])
+def test_component_configuration_status_inference(
+        kb_interface, att_name, att_value, config_name, config_status):
+    if att_value != '':
+        kb_interface.update_attribute_entity(
+            'Attribute',
+            'attribute-name',
+            att_name,
+            'attribute-measurement',
+            att_value)
+    query = f'''
+        match
+            $config isa component-configuration, has component-configuration-name "{config_name}", has component-configuration-status $status;
+            get $status;
+    '''
+    query_result = kb_interface.match_database(query)
+    inferred_status = [
+        status.get("status").get_value() for status in query_result]
+    assert inferred_status[0] == config_status
+
+
+@pytest.mark.parametrize("configurations, c_name, c_required, c_status", [
+    ([('low param', 'unfeasible', 'true'), ('high param', 'unfeasible', 'false')], 'component1', 'false', 'unfeasible'),
+    ([('low param', 'unfeasible', 'true'), ('high param', 'feasible', 'false')], 'component1', 'true', 'configuration error'),
+    ([('low param', 'feasible', 'false'), ('high param', 'feasible', 'false')], 'component1', 'true', 'unsolved'),
+    ([], 'component1', 'true', 'unsolved'),
+    ([('low param', 'feasible', 'true'), ('high param', 'feasible', 'false')], 'component1', 'false', 'feasible'),
+    ([('low param', 'feasible', 'false'), ('high param', 'unfeasible', 'false')], 'component1', 'false', 'feasible'),
+    ([], 'component1', 'false', 'feasible'),
+    ([('low param', 'feasible', 'true'), ('high param', 'feasible', 'false')], 'component1', 'true', 'solved'),
+    ([('low param', 'feasible', 'true'), ('high param', 'unfeasible', 'false')], 'component1', 'true', 'solved'),
+])
+def test_component_status_inference(
+        kb_interface, configurations, c_name, c_required, c_status):
+    for config in configurations:
+        kb_interface.update_attribute_entity(
+            'component-configuration',
+            'component-configuration-name',
+            config[0],
+            'component-configuration-status',
+            "'{}'".format(config[1]))
+        if config[2] == 'true':
+            kb_interface.update_attribute_entity(
+                'component-configuration',
+                'component-configuration-name',
+                config[0],
+                'is-selected',
+                'true')
+    kb_interface.update_attribute_entity(
+        'Component',
+        'component-name',
+        c_name,
+        'is-required',
+        c_required)
+    c_status_inferred = kb_interface.get_attribute_from_entity(
+        'Component',
+        'component-name',
+        c_name,
+        'component-status')
+    print(c_status_inferred)
+    assert all(x in c_status_inferred for x in [c_status]) is True
+
+
 @pytest.mark.parametrize("components, fd_name, fd_selected, fd_status", [
     ([('component2', 'failure')], 'f2_fd1_c2_c3', 'false', 'unfeasible'),
+    ([('component2', 'failure'), ('component3', 'feasible')], 'f2_fd1_c2_c3', 'false', 'unfeasible'),
+    ([('component2', 'failure'), ('component3', 'solved')], 'f2_fd1_c2_c3', 'true', 'unfeasible'),
+    ([('component2', 'failure'), ('component3', 'configuration error')], 'f2_fd1_c2_c3', 'true', 'unfeasible'),
     ([('component2', 'unfeasible')], 'f2_fd1_c2_c3', 'false', 'unfeasible'),
     ([('component2', 'unfeasible')], 'f2_fd1_c2_c3', 'true', 'unfeasible'),
-    ([('component3', 'configuration error')], 'f2_fd1_c2_c3', 'true', 'implict configuration error'),
-    ([('component3', 'configuration error')], 'f2_fd1_c2_c3', 'false', 'ok'),
-    ([('component2', 'failure'), ('component3', 'configuration error')], 'f2_fd1_c2_c3', 'true', 'unfeasible'),
-    ([('component2', 'configuration error'), ('component3', 'ok')], 'f2_fd1_c2_c3', 'true', 'implict configuration error'),
-    ([('component2', 'failure'), ('component3', 'ok')], 'f2_fd1_c2_c3', 'true', 'unfeasible'),
-    ([('component2', 'ok'), ('component3', 'ok')], 'f2_fd1_c2_c3', 'true', 'ok'),
+    ([('component2', 'solved'), ('component3', 'configuration error')], 'f2_fd1_c2_c3', 'true', 'implicit configuration error'),
+    ([('component2', 'configuration error'), ('component3', 'feasible')], 'f2_fd1_c2_c3', 'true', 'implicit configuration error'),
+    ([('component2', 'unsolved'), ('component3', 'unsolved')], 'f2_fd1_c2_c3', 'true', 'unsolved'),
+    ([('component2', 'feasible'), ('component3', 'feasible')], 'f2_fd1_c2_c3', 'true', 'unsolved'),
+    ([('component2', 'solved'), ('component3', 'unsolved')], 'f2_fd1_c2_c3', 'true', 'unsolved'),
+    ([('component2', 'solved'), ('component3', 'feasible')], 'f2_fd1_c2_c3', 'true', 'unsolved'),
+    ([], 'f2_fd1_c2_c3', 'true', 'unsolved'),
+    ([('component2', 'feasible'), ('component3', 'configuration error')], 'f2_fd1_c2_c3', 'false', 'feasible'),
+    ([('component2', 'feasible'), ('component3', 'feasible')], 'f2_fd1_c2_c3', 'false', 'feasible'),
+    ([], 'f2_fd1_c2_c3', 'false', 'feasible'),
+    ([('component2', 'solved'), ('component3', 'solved')], 'f2_fd1_c2_c3', 'true', 'solved'),
 ])
 def test_function_design_status_inference(
      kb_interface, components, fd_name, fd_selected, fd_status):
@@ -51,14 +154,24 @@ def test_function_design_status_inference(
 
 @pytest.mark.parametrize("fds, f_name, f_required, f_status", [
     ([], 'function_no_fd', 'false', 'unfeasible'),
-    ([], 'function1', 'true', 'unsolved'),
-    ([], 'function1', 'false', 'ok'),
     ([('f1_fd1', 'unfeasible', 'false')], 'function1', 'false', 'unfeasible'),
     ([('f1_fd1', 'unfeasible', 'false')], 'function1', 'true', 'unfeasible'),
+    ([('f2_fd1_c2_c3', 'unfeasible', 'true'), ('f2_fd2_c4_c5', 'unfeasible', 'false')], 'function2', 'true', 'unfeasible'),
     ([('f2_fd1_c2_c3', 'unfeasible', 'true')], 'function2', 'true', 'configuration error'),
+    ([('f2_fd1_c2_c3', 'unfeasible', 'true'), ('f2_fd2_c4_c5', 'feasible', 'false')], 'function2', 'true', 'configuration error'),
     ([('f2_fd1_c2_c3', 'implicit configuration error', 'true')], 'function2', 'true', 'implicit configuration error'),
-    ([('f2_fd1_c2_c3', 'implicit configuration error', 'false')], 'function2', 'false', 'ok'),
-    ([('f2_fd1_c2_c3', 'ok', 'true'), ('f2_fd2_c4_c5', 'ok', 'false')], 'function2', 'true', 'ok'),
+    ([('f2_fd1_c2_c3', 'implicit configuration error', 'true'), ('f2_fd2_c4_c5', 'feasible', 'false')], 'function2', 'true', 'implicit configuration error'),
+    ([], 'function1', 'true', 'unsolved'),
+    ([('f2_fd1_c2_c3', 'feasible', 'false'), ('f2_fd2_c4_c5', 'feasible', 'false')], 'function2', 'true', 'unsolved'),
+    ([('f2_fd1_c2_c3', 'feasible', 'false'), ('f2_fd2_c4_c5', 'unfeasible', 'false')], 'function2', 'true', 'unsolved'),
+    ([('f2_fd1_c2_c3', 'unsolved', 'true'), ('f2_fd2_c4_c5', 'feasible', 'false')], 'function2', 'true', 'unsolved'),
+    ([('f2_fd1_c2_c3', 'solved', 'true'), ('f2_fd2_c4_c5', 'feasible', 'false')], 'function2', 'true', 'solved'),
+    ([('f2_fd1_c2_c3', 'solved', 'true'), ('f2_fd2_c4_c5', 'unfeasible', 'false')], 'function2', 'true', 'solved'),
+    ([], 'function1', 'false', 'feasible'),
+    ([('f2_fd1_c2_c3', 'implicit configuration error', 'false')], 'function2', 'false', 'feasible'),
+    ([('f2_fd1_c2_c3', 'implicit configuration error', 'false'), ('f2_fd2_c4_c5', 'unfeasible', 'false')], 'function2', 'false', 'feasible'),
+    ([('f2_fd1_c2_c3', 'feasible', 'false'), ('f2_fd2_c4_c5', 'unfeasible', 'false')], 'function2', 'false', 'feasible'),
+    ([('f2_fd1_c2_c3', 'feasible', 'false'), ('f2_fd2_c4_c5', 'feasible', 'false')], 'function2', 'false', 'feasible'),
 ])
 def test_function_status_inference(
      kb_interface, fds, f_name, f_required, f_status):
@@ -89,3 +202,74 @@ def test_function_status_inference(
         'function-status')
     print(f_status_inferred)
     assert all(x in f_status_inferred for x in [f_status]) is True
+
+
+@pytest.mark.parametrize("functions, tr_name, tr_required, tr_status", [
+    ([('function1', 'unfeasible')], 'task1 requirement', 'false', 'unfeasible'),
+    ([('function1', 'unsolved'), ('function2', 'unfeasible')], 'task1 requirement', 'true', 'unfeasible'),
+    ([('function1', 'configuration error'), ('function2', 'unfeasible')], 'task1 requirement', 'true', 'unfeasible'),
+    ([('function1', 'configuration error')], 'task1 requirement', 'true', 'implicit configuration error'),
+    ([('function1', 'configuration error'), ('function2', 'feasible')], 'task1 requirement', 'true', 'implicit configuration error'),
+    ([('function1', 'configuration error'), ('function2', 'implicit configuration error')], 'task1 requirement', 'true', 'implicit configuration error'),
+    ([('function1', 'implicit configuration error')], 'task1 requirement', 'true', 'implicit configuration error'),
+    ([('function1', 'configuration error'), ('function2', 'solved')], 'task1 requirement', 'true', 'implicit configuration error'),
+    ([('function1', 'configuration error'), ('function2', 'unsolved')], 'task1 requirement', 'true', 'implicit configuration error'),
+    ([('function1', 'unsolved')], 'task1 requirement', 'true', 'unsolved'),
+    ([('function1', 'unsolved'), ('function2', 'solved')], 'task1 requirement', 'true', 'unsolved'),
+    ([('function1', 'unsolved'), ('function2', 'unsolved')], 'task1 requirement', 'true', 'unsolved'),
+    ([('function1', 'feasible')], 'task1 requirement', 'false', 'feasible'),
+    ([('function1', 'unsolved')], 'task1 requirement', 'false', 'feasible'),
+    ([('function1', 'configuration error'), ('function2', 'unsolved')], 'task1 requirement', 'false', 'feasible'),
+    ([('function1', 'configuration error'), ('function2', 'implicit configuration error')], 'task1 requirement', 'false', 'feasible'),
+    ([('function1', 'feasible'), ('function2', 'feasible')], 'task1 requirement', 'false', 'feasible'),
+    ([('function1', 'solved'), ('function2', 'solved')], 'task1 requirement', 'true', 'solved'),
+])
+def test_task_requirement_status_inference(
+     kb_interface, functions, tr_name, tr_required, tr_status):
+    for f in functions:
+        kb_interface.update_attribute_entity(
+            'Function',
+            'function-name',
+            f[0],
+            'function-status',
+            "'{}'".format(f[1]))
+    kb_interface.update_attribute_entity(
+        'task-requirement',
+        'task-requirement-name',
+        tr_name,
+        'is-required',
+        tr_required)
+    tr_status_inferred = kb_interface.get_attribute_from_entity(
+        'task-requirement',
+        'task-requirement-name',
+        tr_name,
+        'task-requirement-status')
+    print(tr_status_inferred)
+    assert all(x in tr_status_inferred for x in [tr_status]) is True
+
+
+@pytest.mark.parametrize("tr_name, t_name, status", [
+    ('task1 requirement', 'task1', 'unfeasible'),
+    ('task1 requirement', 'task1', 'implicit configuration error'),
+    ('task1 requirement', 'task1', 'unsolved'),
+    ('task1 requirement', 'task1', 'feasible'),
+    ('task1 requirement', 'task1', 'solved'),
+])
+def test_task_status_inference(kb_interface, tr_name, t_name, status):
+    kb_interface.update_attribute_entity(
+        'task-requirement',
+        'task-requirement-name',
+        tr_name,
+        'task-requirement-status',
+        "'{}'".format(status))
+    tr_status_inferred = kb_interface.get_attribute_from_entity(
+        'task-requirement',
+        'task-requirement-name',
+        tr_name,
+        'task-requirement-status')
+    t_status_inferred = kb_interface.get_attribute_from_entity(
+        'Task',
+        'task-name',
+        t_name,
+        'task-requirement-status')
+    assert tr_status_inferred == t_status_inferred
