55f18473c40b11b3ece5229ab66b7c5a0dd20b62
Gustavo <g.rezendesilva@tudelft.nl>
2023-05-09 13:31:50 +0200

    🔥 remove commented code

--- src/model_interface.py
+++ src/model_interface.py
@@ -28,14 +28,6 @@ class ModelInterface(TypeDBInterface):
         # self.component_ordering_funcs = component_ordering_funcs
         # self.default_component_ordering_func = default_component_ordering_func
 
-    # Add a task
-    # def add_task(self, task_name):
-    #     return self.insert_entity('Task', 'task-name', task_name)
-    #
-    # # Remove a task
-    # def remove_task(self, task_name):
-    #     return self.delete_entity('Task', 'task-name', task_name)
-
     # Request task
     def request_task(self, task_name):
         return self.update_attribute_entity(
@@ -45,43 +37,6 @@ class ModelInterface(TypeDBInterface):
     def cancel_task(self, task_name):
         return self.update_attribute_entity(
             'Task', 'task-name', task_name, 'is-required', 'false')
-
-    # Get tasks that have the "unsolved" status raw
-    # def get_required_unsolved_tasks_raw(self):
-    #     query = f'''
-    #         match
-    #             $task isa Task, has task-name $task-name,
-    #                 has is-required true, has task-status 'unsolved';
-    #             get $task-name;
-    #     '''
-    #     return self.match_database(query)
-    #
-    # # Get tasks that don't have the "solved" status
-    # def get_required_unsolved_tasks(self):
-    #     query_result = self.get_required_unsolved_tasks_raw()
-    #     return [result.get("task-name").get_value() for result in query_result]
-
-    # Get tasks that don't have the "solved" status raw
-    # def get_required_tasks_not_solved_raw(self):
-    #     query = f'''
-    #         match
-    #             $task isa Task, has task-name $task-name,
-    #                 has is-required true;
-    #             not {{$task isa Task, has is-required true,
-    #                 has task-status $task-status;
-    #                 $task-status = "solved";}};
-    #             get $task-name;
-    #     '''
-    #     return self.match_database(query)
-    #
-    # # Get tasks that don't have the "solved" status
-    # def get_required_tasks_not_solved(self):
-    #     query_result = self.get_required_tasks_not_solved_raw()
-    #     return [result.get("task-name").get_value() for result in query_result]
-
-    # def has_required_tasks_not_solved(self):
-    #     tasks_not_solved = self.get_required_tasks_not_solved()
-    #     return True if len(tasks_not_solved) > 0 else False
 
     # Update status of a task
     def update_task_status(self, task_name, task_status):
@@ -105,44 +60,6 @@ class ModelInterface(TypeDBInterface):
         status = self.get_attribute_from_entity(
              'Task', 'task-name', task_name, 'task-status')
         return all(x in status for x in ['feasible'])
-
-    # TODO: replace for implicit-task-requirement???
-    # TODO: I think it might be possible to just return a list with all
-    # functions, no need for a dict
-    # def get_functional_hierarchy_from_task(self, task_name):
-    #     root_functions = self.get_root_functions_from_task(task_name)
-    #     return {f: [f] + self.get_child_functions(f) for f in root_functions}
-
-    # Get all Functions in a task-requirement relationship with a Task raw
-    # def get_required_functions_from_task_raw(self, task_name):
-    #     query = f'''
-    #         match
-    #             $task isa Task, has task-name "{task_name}";
-    #             (task:$task, required-function:$function) isa task-requirement;
-    #             $function isa Function, has function-name $function-name;
-    #             get $function-name;
-    #     '''
-    #     return self.match_database(query)
-    #
-    # # Get all Functions in a task-requirement relationship with a Task
-    # def get_required_functions_from_task(self, task_name):
-    #     query_result = self.get_required_functions_from_task_raw(task_name)
-    #     return [r.get("function-name").get_value() for r in query_result]
-
-    # def get_child_functions_raw(self, function):
-    #     query = f'''
-    #         match
-    #             $function isa Function, has function-name "{function}";
-    #             (parent-function:$function, child-function:$fc)
-    #                 isa implicit-functional-hierarchy;
-    #             $fc isa Function, has function-name $fc-name;
-    #             get $fc-name;
-    #     '''
-    #     return self.match_database(query)
-    #
-    # def get_child_functions(self, function):
-    #     query_result = self.get_child_functions_raw(function)
-    #     return [result.get("fc-name").get_value() for result in query_result]
 
     # Get all Functions with is-required property equal to True raw
     def get_required_functions_raw(self):
@@ -402,129 +319,3 @@ class ModelInterface(TypeDBInterface):
             name,
             'is-active',
             value)
-    # def get_functions_not_required_anymore_raw(self):
-    #     query = f'''
-    #         match
-    #             $function isa Function, has is-function-required true,
-    #                 has function-name $function-name;
-    #             $task isa Task;
-    #             {{(task:$task, required-function:$function)
-    #                 isa implicit-task-requirement;}} or
-    #             {{(task:$task, required-function:$function)
-    #                 isa task-requirement;}};
-    #             not {{$task isa Task, has is-task-required true;}};
-    #             get $function-name;
-    #     '''
-    #     return self.match_database(query)
-    #
-    # def get_functions_not_required_anymore(self):
-    #     query_result = self.get_functions_not_required_anymore_raw()
-    #     return [r.get("function-name").get_value() for r in query_result]
-    #
-    # def get_component_types_not_required_anymore_raw(self):
-    #     query = f'''
-    #         match
-    #             $component isa ComponentType,
-    #                 has is-component-type-required true,
-    #                 has component-type $component-type;
-    #             (function:$function, required-component:$component)
-    #                 isa function-design;
-    #             not {{$function isa Function, has is-function-required true;}};
-    #             get $component-type;
-    #     '''
-    #     return self.match_database(query)
-    #
-    # def get_component_types_not_required_anymore(self):
-    #     query_result = self.get_component_types_not_required_anymore_raw()
-    #     return [r.get("componet-type").get_value() for r in query_result]
-    #
-    # def get_components_not_required_anymore_raw(self):
-    #     query = f'''
-    #         match
-    #             $component isa Component, has is-component-required true,
-    #                 has component-name $component-name;
-    #             {{(function:$function, required-component:$component)
-    #                 isa function-design;
-    #                 not {{$function isa Function,
-    #                     has is-function-required true;}};}} or
-    #             {{(componentType:$component-type, component:$component)
-    #                 isa component-design;
-    #                 not {{$component-type isa ComponentType,
-    #                     has is-component-type-required true;}};}};
-    #             get $component-name;
-    #     '''
-    #     return self.match_database(query)
-    #
-    # def get_components_not_required_anymore(self):
-    #     query_result = self.get_components_not_required_anymore_raw()
-    #     return [r.get("component-name").get_value() for r in query_result]
-    #
-
-
-    # # get components from fd (only dealing with Components, not ComponentType)
-    # def get_components_from_function_design_raw(self, fd_name):
-    #     query = f'''
-    #         match
-    #             $fd (function:$f, required-component:$component)
-    #                 isa function-design,  has function-design-name "{fd_name}";
-    #             $component isa Component, has component-name $component-name;
-    #             get $component-name;
-    #     '''
-    #     return self.match_database(query)
-    #
-    # def get_components_from_function_design(self, fd_name):
-    #     query_result = self.get_components_from_function_design_raw(fd_name)
-    #     return [r.get("component-name").get_value() for r in query_result]
-    #
-    # def get_component_types_from_function_design_raw(self, fd_name):
-    #     query = f'''
-    #         match
-    #             $fd (function:$f, required-component:$c)
-    #                 isa function-design,  has function-design-name "{fd_name}";
-    #             $c isa ComponentType, has component-type $component-type;
-    #             get $component-type;
-    #     '''
-    #     return self.match_database(query)
-    #
-    # def get_component_types_from_function_design(self, fd_name):
-    #     result = self.get_component_types_from_function_design_raw(fd_name)
-    #     return [r.get("component-type").get_value() for r in result]
-    #
-    # def component_order_asc(self, component_type):
-    #     query = f'''
-    #         match
-    #             $ct isa ComponentType, has component-type "{component_type}";
-    #             $cd (componentType:$ct, component:$component)
-    #                 isa component-design,
-    #                 has component-design-priority $priority;
-    #             $component isa Component, has component-name $component-name;
-    #             get $component-name, $priority;
-    #             sort $priority asc;
-    #     '''
-    #     return self.match_database(query)
-    #
-    # def component_order_desc(self, component_type):
-    #     query = f'''
-    #         match
-    #             $ct isa ComponentType, has component-type "{component_type}";
-    #             $cd (componentType:$ct, component:$component)
-    #                 isa component-design,
-    #                 has component-design-priority $priority;
-    #             $component isa Component, has component-name $component-name;
-    #             get $component-name, $priority;
-    #             sort $priority desc;
-    #     '''
-    #     return self.match_database(query)
-    #
-    # def get_components_from_component_type_raw(self, component_type):
-    #     _func = None
-    #     if component_type in self.component_ordering_funcs:
-    #         _func = getattr(
-    #             self, self.component_ordering_funcs[component_type])
-    #     else:
-    #         _func = getattr(self, self.default_component_ordering_func)
-    #     return _func(component_type)
-    #
-    # def get_components_from_component_type(self, component_type):
-    #     result = self.get_components_from_component_type_raw(component_type)
-    #     return [r.get("component-name").get_value() for r in result]
